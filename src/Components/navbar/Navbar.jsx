0096efde1f20273626353acfcb9a3b85949f14ac
sunnat <sunnattoxtaqulov4444@gmail.com>
2024-05-07 15:54:24 +0300

    navbar add project

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,18 +1,26 @@
+import { useState } from 'react'
+import { close, logo, menu } from '../../assets'
 import { navigationLinks } from '../../util/const'
 import { styles } from '../../util/style'
 import './Navbar.css'
 const Navbar = () => {
+    const [toggleNav , settoggleNav] = useState(true)
+
   return (
     <div className={`w-full py-6  navbar ${styles.flexBetween} `}>
         {/* Logo  */}
-        <div className={`${styles.heading}`}>Logo</div>
-        <ul>
+        <img src={logo} alt="logo" className='w-[130px] h-[35px]  cursor-pointer ' />
+        <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
 {
     navigationLinks.map((item, idx) =>(
-        <li>{item?.title}</li>
+        <li key={item?.Id} className={`font-montserrat font-normal cursor-pointer text-[16px] text-white ${idx == navigationLinks.length - 1 ? "mr-0" : "mr-10"} hover:text-lightWhite transition-all duration-500`}>{item?.title}</li>
     ))
 }
         </ul>
+        {/* Navigation btn */}
+        <div className={`sm:hidden flex flex-1 justify-end items-center `}>
+            <img src={toggleNav ? close : menu } alt="menu" />
+        </div>
     </div>
   )
 }
