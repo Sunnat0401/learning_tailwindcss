2d6ad2cc1301be794f69f44383094b6df5bf0187
sunnat <sunnattoxtaqulov4444@gmail.com>
2024-05-07 13:24:13 +0300

    learning padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import { styles } from "./util/style"
 const App = () => {
   return (
    <div className="bg-primary p-10 w-full overflow-hidden" > 
-   <div className="sm:px-16 px-6  py-5 color-lightWhite">
+   <div className={`${styles.paddingX} ${styles.paddingY} ${styles.marginY}`}>
     <p className="text-white">Navbar</p>
     </div>
-    
+      <div className={`bg-slate-500 ${styles.padding} `}></div>
     </div>
   )
 }
