7bb9aeba2c8b5fddd80fa3ce70601b5487e1e696
sunnat <sunnattoxtaqulov4444@gmail.com>
2024-05-10 14:59:30 +0300

    everything didn't work well in the background

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Navbar } from "./Components"
+import { Home, Navbar } from "./Components"
 import { styles } from "./util/style"
 const App = () => {
   return (
    <div className="bg-primary p-10 w-full overflow-hidden" > 
-   {/* <div className={`${styles.paddingX} `}>
-    <div className={`${styles.container} ${styles.flexCenter}`}>
-      <div className={`${styles.heading}`}>
-       
+   <div className={`${styles.paddingX}${styles.flexCenter} `}>
+    <div className={`${styles.container} `}>
+     <Navbar/>
+    </div>
+    </div>
+    <div className={`bg-primary  ${styles.flexStart}`}>
+      <div className={`${styles.container}`}>
+     <Home/>
       </div>
     </div>
-    </div> */}
-     <Navbar/>
     </div>
   )
 }
